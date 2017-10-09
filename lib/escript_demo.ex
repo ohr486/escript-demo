1f937d40ce8fc2216efe9a0c19d2984ed2032721
ohr486 <ohr486@gmail.com>
2017-10-09 19:00:06 +0900

    add base files

---
+++ lib/escript_demo.ex
@@ -0,0 +1,12 @@
+defmodule EscriptDemo do
+  @moduledoc """
+  Documentation for EscriptDemo.
+  """
+
+  @doc """
+  Escript main func
+  """
+  def main(args) do
+    IO.puts "Hello, EscriptDemo!"
+  end
+end
